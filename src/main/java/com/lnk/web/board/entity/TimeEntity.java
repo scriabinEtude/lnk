eb860a1c253d0e5372df7d0ec317a612594b08e2
Kim Young Jin <wndrhtkfurh@naver.com>
2020-06-03 23:27:12 +0900

    Commit Test

--- src/main/java/com/lnk/web/board/entity/TimeEntity.java
+++ src/main/java/com/lnk/web/board/entity/TimeEntity.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public abstract class TimeEntity {
     /* Entity가 처음 저장될때 생성일을 주입하는 어노테이션
        이때 생성일은 update할 필요가 없으므로, updatable = false 속성 추가
-       속성을 추가하지 않으면 수정 시, 해당 값은 null이 되어버림 */
+       속성을 추가하지 않으면 수정 시, 해당 값은 null이 되어버린다. */
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdDate;
