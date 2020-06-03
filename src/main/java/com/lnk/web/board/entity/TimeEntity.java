457892f8801567d324ebfb8684361b4bd72bc4b1
Kim Young Jin <wndrhtkfurh@naver.com>
2020-06-03 23:33:59 +0900

    Commit Test

--- src/main/java/com/lnk/web/board/entity/TimeEntity.java
+++ src/main/java/com/lnk/web/board/entity/TimeEntity.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 //JPA에게 해당 Entity는 Auditing기능을 사용한다는 것을 알리는 어노테이션입
 @EntityListeners(AuditingEntityListener.class)
-public abstract class TimeEntity {
+public abstract class TimeEntity   {
     /* Entity가 처음 저장될때 생성일을 주입하는 어노테이션
        이때 생성일은 update할 필요가 없으므로, updatable = false 속성 추가
        속성을 추가하지 않으면 수정 시, 해당 값은 null이 되어버린다. */
